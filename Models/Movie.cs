1370f1d0d68546b58c525ca70c07eddc9ba5b27e
dancingball <lionmarksham@gmail.com>
2022-11-09 13:59:34 -0500

    added validations....

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -5,12 +5,20 @@ namespace ASPNETRAZORPAGES.Models;
 
 public class Movie {
     public int Id { get; set; }
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
     public string? Title { get; set;}
-    [Display(Name = "Release Date")]
-    [DataType(DataType.Date)]
+    
+    [Display(Name = "Release Date"), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
     public DateTime ReleaseDate { get; set; }
-
+    
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
     public string? Genre { get; set; }
+
+    [Range(1, 100), DataType(DataType.Currency)]
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
+    public string Rating { get; set; } = string.Empty;
 }
--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ASPNETRAZORPAGES.Models;
@@ -20,13 +21,33 @@ namespace RazorPagesMovie.Pages_Movies
         }
 
         public IList<Movie> Movie { get;set; } = default!;
+        
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+        
+        public SelectList? Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? MovieGenre { get; set; }
 
         public async Task OnGetAsync()
         {
-            if (_context.Movie != null)
-            {
-                Movie = await _context.Movie.ToListAsync();
+
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var movies = from m in _context.Movie
+                        select m;
+            if(!string.IsNullOrEmpty(SearchString)) {
+                movies = movies.Where(x => x.Title.Contains(SearchString));
             }
+
+            if(!string.IsNullOrEmpty(MovieGenre)) {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
         }
     }
 }
