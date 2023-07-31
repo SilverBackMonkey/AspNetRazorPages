fdfa8f2801f65ce54371de22b12d46d1740592cb
steadyblinder <steadyblinder@gmail.com>
2023-07-31 02:33:23 -0400

    added CRUD operation...

---
+++ Data/RazorPagesMovieContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ASPNETRAZORPAGES.Models;
+
+namespace RazorPagesMovie.Data
+{
+    public class RazorPagesMovieContext : DbContext
+    {
+        public RazorPagesMovieContext (DbContextOptions<RazorPagesMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ASPNETRAZORPAGES.Models.Movie> Movie { get; set; } = default!;
+    }
+}
